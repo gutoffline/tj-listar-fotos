63f843bde1b2fc07ad647a2aa5a225309f96d6cd
Guto Xavier <gutoffline@gmail.com>
2020-01-24 21:56:11 -0300

    Fotos

---
+++ Fotos/Assets/Fotos.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class Fotos : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public List<string> fotos = new List<string>();
+    public int indice = 0;
+
+    public void Listar(){
+        Debug.Log("Carregar Foto");
+        
+        DirectoryInfo dir = new DirectoryInfo("C:/fotos/");
+        FileInfo[] info = dir.GetFiles("*.*");
+        foreach (FileInfo f in info){
+            fotos.Add(f.ToString());
+        }
+
+        foreach (string item in fotos){
+            Debug.Log(item);
+        }
+
+        byte[] byteArray = File.ReadAllBytes(@fotos[indice]);
+        Texture2D sampleTexture = new Texture2D(2,2);
+        bool isLoaded = sampleTexture.LoadImage(byteArray);
+        GameObject image = GameObject.Find("RawImage");
+        if (isLoaded)
+        {
+            image.GetComponent<RawImage>().texture = sampleTexture;
+        }
+    }
+
+
+    public void Anterior(){
+        indice--;
+        byte[] byteArray = File.ReadAllBytes(@fotos[indice + 1]);
+        Texture2D sampleTexture = new Texture2D(2,2);
+        bool isLoaded = sampleTexture.LoadImage(byteArray);
+        GameObject image = GameObject.Find("RawImage");
+        if (isLoaded)
+        {
+            image.GetComponent<RawImage>().texture = sampleTexture;
+        }
+    }
+
+    public void Proxima(){
+        indice++;
+        byte[] byteArray = File.ReadAllBytes(@fotos[indice + 1]);
+        Texture2D sampleTexture = new Texture2D(2,2);
+        bool isLoaded = sampleTexture.LoadImage(byteArray);
+        GameObject image = GameObject.Find("RawImage");
+        if (isLoaded)
+        {
+            image.GetComponent<RawImage>().texture = sampleTexture;
+        }
+    }
+}
